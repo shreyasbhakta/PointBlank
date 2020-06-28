94050c8f65a0170fade7a78ef4bab9df037d1c41
Vishwa-Raghavendra <42895647+Vishwa-Raghavendra@users.noreply.github.com>
2020-06-28 20:50:21 +0530

    Initial commit

---
+++ app/src/main/java/com/dscepointblank/pointblank/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.dscepointblank.pointblank.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dscepointblank.pointblank.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
