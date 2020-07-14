7629286e77b2464c78dec8d9c83a36b6b34e0435
Vishwa-Raghavendra <42895647+Vishwa-Raghavendra@users.noreply.github.com>
2020-07-15 01:46:53 +0530

    Login screen implemented

--- app/src/main/java/com/dscepointblank/pointblank/fragments/LoginFragment.kt
+++ app/src/main/java/com/dscepointblank/pointblank/fragments/LoginFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.text.HtmlCompat
 import com.dscepointblank.pointblank.R
+import kotlinx.android.synthetic.main.fragment_login.view.*
 
 
 class LoginFragment : Fragment() {
@@ -16,6 +18,11 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val view =  inflater.inflate(R.layout.fragment_login, container, false)
+
+        val text = "<font color=#00C853>Point</font> <font color=#fafafa>Blank</font>"
+        view.loginPBTV.text = HtmlCompat.fromHtml(text,HtmlCompat.FROM_HTML_MODE_LEGACY)
+
+        return view
     }
 }
